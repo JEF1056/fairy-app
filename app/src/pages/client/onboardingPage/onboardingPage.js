7c2b25f702f5f533e80b54bd17645e6c884afeae
alisunnyday <calpurniahepburn@gmail.com>
2023-02-04 15:55:50 -0800

    alison update 4pm

--- app/src/pages/client/onboardingPage/onboardingPage.js
+++ app/src/pages/client/onboardingPage/onboardingPage.js
@@ -14,7 +14,7 @@ function OnBoardingPage() {
         <li class="step step-primary">Personal Information</li>
         <li class="step step-primary">Medical History</li>
         <li class="step">Insurance Information</li>
-        <li class="step">Receive Product</li>
+        <li class="step">Review and Submit</li>
       </ul>
       <br></br>
       Legal Name:
@@ -51,6 +51,42 @@ function OnBoardingPage() {
         <option>Non-binary</option>
       </select>
       <br></br>
+      Race: (check all that apply)
+      <br></br>
+      <input type="checkbox" class="checkbox-xs" />
+      <span class="label-text"> American Indian or Alaska Native</span>
+      <br></br>
+      <input type="checkbox" class="checkbox-xs" />
+      <span class="label-text"> Asian</span>
+      <br></br>
+      <input type="checkbox" class="checkbox-xs" />
+      <span class="label-text"> Black or African American</span>
+      <br></br>
+      <input type="checkbox" class="checkbox-xs" />
+      <span class="label-text"> White</span>
+      <br></br>
+      <input type="checkbox" class="checkbox-xs" />
+      <span class="label-text"> Decline to Provide</span>
+      <br></br>
+
+      Ethnicity:
+      <br></br>
+      <input type="checkbox" class="checkbox-xs" />
+      <span class="label-text"> Hispanic or Latino</span>
+      <br></br>
+      <input type="checkbox" class="checkbox-xs" />
+      <span class="label-text"> Not Hispanic or Latino</span>
+      <br></br>
+      <input type="checkbox" class="checkbox-xs" />
+      <span class="label-text"> Decline to Provide</span>
+      <br></br>
+
+    Phone:
+    <input
+      type="text"
+      placeholder="(XXX)-XXX-XXXX"
+      class="input w-full max-w-xs"
+    />
       Address:
       <br></br>
       <input
@@ -82,12 +118,62 @@ function OnBoardingPage() {
     <br></br>
     Emergency Contact Information:
     <br></br>
+    Name:
     <input
       type="text"
       placeholder="Name"
       class="input w-full max-w-xs"
     />
-    
+    Phone:
+    <input
+      type="text"
+      placeholder="(XXX)-XXX-XXXX"
+      class="input w-full max-w-xs"
+    />
+    <br></br>
+    Relationship to Patient:
+    <input
+      type="text"
+      placeholder="Type here"
+      class="input w-full max-w-xs"
+    />
+    <br></br>
+    Address:
+      <br></br>
+      <input
+        type="text"
+        placeholder="Address Line 1"
+        class="input w-full max-w-xs"
+      />
+      <input
+        type="text"
+        placeholder="Address Line 2"
+        class="input w-full max-w-xs"
+      />
+      <br></br>
+      <input
+        type="text"
+        placeholder="City"
+        class="input w-full max-w-xs"
+      />
+      <input
+        type="text"
+        placeholder="State"
+        class="input w-full max-w-xs"
+      />
+      <input
+      type="text"
+      placeholder="ZIP"
+      class="input w-full max-w-xs"
+    />
+    <br></br>
+
+
+
+
+
+    <br></br>
+    <button class="btn">Next</button>
     </>
   );
 }
