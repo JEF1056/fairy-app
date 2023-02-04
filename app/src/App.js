3b19bf81b65206b5d64530c7c44be655fc77e013
Brian Nguyen <86499417+briankhoi@users.noreply.github.com>
2023-02-03 21:27:23 -0800

    changed folder names and event page added

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
-import HomePage from "./pages/client/homepage";
+import HomePage from "./pages/client/homepage/homePage";
+import EventsPage from "./pages/client/eventspage/eventsPage";
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
+    element: <HomePage />, 
   },
+  {
+    path: "/events",
+    element: <EventsPage />,
+  }
 ]);
 
 function App() {
---
+++ app/src/pages/client/eventspage/eventsPage.js
@@ -0,0 +1,42 @@
+import React from "react";
+import HeaderComponent from "../../../components/header";
+import FooterComponent from "../../../components/footer";
+import NavRow from "../../../components/navRow";
+import {
+  faArrowRightArrowLeft,
+  faCapsules,
+} from "@fortawesome/free-solid-svg-icons";
+
+
+//use local storage to store list of events, then parse and loop through it
+//localStorage.setItem('myCat', 'Tom'); [0] key, [1] val
+//
+//add event function - add event key, func should read from event key to add on new event and then write back to the event key and then page should read and then loop through this event key
+
+function EventsPage() {
+
+
+  return (
+    <>
+      <HeaderComponent />
+
+      <div class="h-max pb-16">
+        <NavRow
+          icon={faArrowRightArrowLeft}
+          title="Export Data"
+          description="Transfer from one medical provider to another"
+        />
+
+        <NavRow
+          icon={faCapsules}
+          title="View Medications"
+          description="See prescriptions and medication schedule"
+        />
+      </div>
+
+      <FooterComponent />
+    </>
+  );
+}
+
+export default EventsPage;
