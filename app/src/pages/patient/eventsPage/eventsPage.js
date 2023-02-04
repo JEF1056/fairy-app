55347152b71c6e34f93d65f07161ad8f90977b29
Brian Nguyen <86499417+briankhoi@users.noreply.github.com>
2023-02-04 09:37:56 -0800

    merging changes

--- app/src/pages/patient/eventsPage/eventsPage.js
+++ app/src/pages/patient/eventsPage/eventsPage.js
@@ -2,9 +2,12 @@ import React from "react";
 import HeaderComponent from "../../../components/header";
 import FooterComponent from "../../../components/footer";
 import NavRow from "../../../components/navRow";
+import secureLocalStorage from "react-secure-storage";
+
 import {
   faArrowRightArrowLeft,
   faCapsules,
+  faNotesMedical,
 } from "@fortawesome/free-solid-svg-icons";
 
 //use local storage to store list of events, then parse and loop through it
@@ -12,12 +15,36 @@ import {
 //
 //add event function - add event key, func should read from event key to add on new event and then write back to the event key and then page should read and then loop through this event key
 
+function addEvent(eventJson) {
+    var event = JSON.parse(eventJson)   
+    console.log(event)
+    secureLocalStorage.setItem("testkey", event)
+}
+
+
+
+
 function EventsPage() {
+  addEvent('{"result":true, "count":42}')
+  //getitem
+
+  var storage = "[1,2,3]"
+
+  
   return (
+    //loop through all events
+    //get their individual value
     <>
       <HeaderComponent />
 
       <div class="h-max pb-16">
+
+        {
+          JSON.parse(storage, (key) => {
+            <p>{key}</p>
+          })
+        }
+
         <NavRow
           icon={faArrowRightArrowLeft}
           title="Export Data"
@@ -28,6 +55,12 @@ function EventsPage() {
           icon={faCapsules}
           title="View Medications"
           description="See prescriptions and medication schedule"
+        />
+
+        <NavRow
+            icon={faNotesMedical}
+            title = "Test Alert"
+            description = "Test desc"
         />
       </div>
 
