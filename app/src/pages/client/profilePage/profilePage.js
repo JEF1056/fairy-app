bc39be03f7a07bab0a4ceae8edfb85823f19ab02
Brian Nguyen <86499417+briankhoi@users.noreply.github.com>
2023-02-05 07:29:35 -0800

    fake profile, need to fix responsiveness

--- app/src/pages/client/profilePage/profilePage.js
+++ app/src/pages/client/profilePage/profilePage.js
@@ -20,7 +20,11 @@ function ProfilePage() {
           <div class="card-body items-center text-center">
             <h1 class="card-title">Jess Fan</h1>
             <div class="overflow-x-auto">
-              <table class="table table-compact w-full">
+              <table class="table table-compact">
+                <br></br>
+                <thead>
+                  <th>Personal Information</th>
+                </thead>
                 <tbody>
                   <tr>
                     <th>Sex</th>
@@ -43,8 +47,64 @@ function ProfilePage() {
                     <td>111 CruzHacks Way</td>
                   </tr>
                   <tr>
-                    <th>Emergency Contact Phone Number</th>
-                    <td>111-111-1111</td>
+                    <th>Emergency Contact</th>
+                    <td>408-131-5829</td>
+                  </tr>
+                </tbody>
+                <br></br>
+                <br></br>
+                <thead>
+                  <th>Medical Information</th>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th>Allergies</th>
+                    <td>Peanuts, Pollen, and Soy</td>
+                  </tr>
+                  <tr>
+                    <th>Medical History</th>
+                    <td>ADD/ADHD, Asthma, COVID-19</td>
+                  </tr>
+                  <tr>
+                    <th>Current Medications</th>
+                    <td>None</td>
+                  </tr>
+                  <tr>
+                    <th>Dietary Restrictions</th>
+                    <td>Peanuts, Soy, Lactose, Meat</td>
+                  </tr>
+                </tbody>
+                <br></br>
+                <br></br>
+                <thead>
+                  <th>Insurance Information</th>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th>Insurance Carrier</th>
+                    <td>Anthem Blue Cross</td>
+                  </tr>
+                  <tr>
+                    <th>Policy Number</th>
+                    <td>12091342</td>
+                  </tr>
+                  <tr>
+                    <th>Group Number</th>
+                    <td>02-90700</td>
+                  </tr>
+                  <tr>
+                    <th>Insured's Name</th>
+                    <td>Jess Fan</td>
+                  </tr>
+                  <tr>
+                    <th>Member ID</th>
+                    <td>BJX34322</td>
+                  </tr>
+                  <tr>
+                    <th>Insurance Card</th>
+                    <td>
+                      <img src="#"></img>
+                    </td>
                   </tr>
                 </tbody>
               </table>
