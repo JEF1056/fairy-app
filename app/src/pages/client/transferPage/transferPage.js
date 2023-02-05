619a3dfa9ca74069ee355ddae9de13b20e6d0862
Jess <jessefan2002@gmail.com>
2023-02-04 21:39:34 -0800

    update transferPage

--- app/src/pages/client/transferPage/transferPage.js
+++ app/src/pages/client/transferPage/transferPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import HeaderComponent from "../../../components/client/header";
-import FooterComponent from "../../../components/client/footer";    
+import FooterComponent from "../../../components/client/footer";
 import QRCode from "react-qr-code";
 import { useRecoilCallback, useRecoilValue } from "recoil";
 import { transferCode } from "../../../util/transferCodes";
@@ -13,6 +13,33 @@ function TransferPage() {
   const callbackTransferCode = useRecoilCallback(({ set }) => () => {
     set(transferCode, uuidv4());
   });
+
+  var fakeData = [
+    {
+      image: "/jess.png",
+      name: "Jess",
+      company: "🅱️oogle",
+      role: "Computer Doctor, fixer of random bugs",
+    },
+    {
+      image: "/brian.jpg",
+      name: "Brian",
+      company: "❌D",
+      role: "Professional mind boggler",
+    },
+    {
+      image: "/alison.png",
+      name: "Alison",
+      company: "Your mom",
+      role: "Roast master",
+    },
+    {
+      image: "/aisha.jpg",
+      name: "Aisha",
+      company: "MediFae, inc.",
+      role: "Biggest brain",
+    },
+  ];
 
   function GetTransferCode() {
     return JSON.stringify({ uuid: GetUserID(), code: getTransferCode });
@@ -35,9 +62,49 @@ function TransferPage() {
               callback={callbackTransferCode}
             />
             <p>Show this code to your medical provider.</p>
-            <p>{GetTransferCode()}</p>
           </div>
         </div>
+      </div>
+
+      <div class="overflow-x-auto w-full">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Job</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {fakeData.map((obj) => (
+              <tr>
+                <td>
+                  <div class="flex items-center space-x-3">
+                    <div class="avatar">
+                      <div class="mask mask-squircle w-12 h-12">
+                        <img
+                          src={obj.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div class="font-bold">{obj.name}</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  {obj.company}
+                  <br />
+                  <span class="badge badge-ghost badge-sm">{obj.role}</span>
+                </td>
+                <th>
+                  <button class="btn btn-ghost btn-xs">details</button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
 
       <FooterComponent />
